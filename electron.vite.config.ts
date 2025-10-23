524baac2fad682ffef6c0f3c57b0716f4126c43e
Sam <samuel.s.mamane@gmail.com>
2025-10-24 00:10:47 +0200

    build: update version and electron builder config
    - Bump package version to 1.0.33
    - Update electron builder appId and add mac distribution settings
    - Reorder imports in electron vite config

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig, externalizeDepsPlugin } from "electron-vite";
-import react from "@vitejs/plugin-react";
-
 import { resolve } from "node:path";
+import react from "@vitejs/plugin-react";
+import { defineConfig, externalizeDepsPlugin } from "electron-vite";
 
 export default defineConfig({
 	main: {
