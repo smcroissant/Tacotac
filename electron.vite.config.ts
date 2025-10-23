70eef1c8cee69259d420b83d30cdb2d4710cf35e
Sam <samuel.s.mamane@gmail.com>
2025-10-23 23:16:20 +0200

    build: update version and configuration files
    - Update package version from 1.0.14 to 1.0.25
    - Move build configuration from package.json to electron-builder.yml
    - Remove tailwindcss plugin from vite config
    - Change publish provider from github to generic

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,27 +1,20 @@
-import { resolve } from 'path'
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { resolve } from "path";
+import { defineConfig, externalizeDepsPlugin } from "electron-vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  main: {
-    build: {
-      outDir: 'out/main',
-    },
-    plugins: [externalizeDepsPlugin()]
-  },
-  preload: {
-    build: {
-      outDir: 'out/preload'
-    },
-    plugins: [externalizeDepsPlugin()]
-  },
-  renderer: {
-    resolve: {
-      alias: {
-        '@renderer': resolve('src/renderer/src')
-      }
-    },
-    plugins: [react(), tailwindcss(),]
-  },
-})
+	main: {
+		plugins: [externalizeDepsPlugin()],
+	},
+	preload: {
+		plugins: [externalizeDepsPlugin()],
+	},
+	renderer: {
+		resolve: {
+			alias: {
+				"@renderer": resolve("src/renderer/src"),
+			},
+		},
+		plugins: [react()],
+	},
+});
